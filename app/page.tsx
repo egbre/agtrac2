a0ef5490d19130d594ade8cfd47fa30ec360ffd9
egbre <40549279+egbre@users.noreply.github.com>
2023-11-05 23:06:19 -0600

    Update page.tsx
    again

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,6 @@ export default async function Home() {
           <p className="text-sm font-semibold text-[#1d9bf0]">
             Introducing AgTrac
           </p>
-        </a>
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
