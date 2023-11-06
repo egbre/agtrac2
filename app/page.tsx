8ad8811ae1d26f6ab7aa6f713b5f71fdb66bf8af
egbre <40549279+egbre@users.noreply.github.com>
2023-11-05 23:09:28 -0600

    Update page.tsx
    get rid of legacy twitter stuffs

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,6 @@ export default async function Home() {
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-          <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing AgTrac
-          </p>
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
