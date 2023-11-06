b7faad8252983fdd1e2ec8f0b1325e22637f0f1b
egbre <40549279+egbre@users.noreply.github.com>
2023-11-05 22:54:27 -0600

    Update page.tsx
    get rid of twitter

--- app/page.tsx
+++ app/page.tsx
@@ -27,13 +27,6 @@ export default async function Home() {
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <a
-          href="https://twitter.com/steventey/status/1613928948915920896"
-          target="_blank"
-          rel="noreferrer"
-          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
           <p className="text-sm font-semibold text-[#1d9bf0]">
             Introducing AgTrac
           </p>
